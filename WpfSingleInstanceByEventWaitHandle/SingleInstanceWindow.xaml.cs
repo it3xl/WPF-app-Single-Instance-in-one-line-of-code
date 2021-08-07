840ade3e20f2640f6f24a863b2e6f7d8681cd4b1
Ilya Tretyakov #it3xl <it3xl@users.noreply.github.com>
2021-08-07 09:36:44 +0300

    Renaming of the main window. Minor formatting.

---
+++ WpfSingleInstanceByEventWaitHandle/SingleInstanceWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+
+namespace WpfSingleInstanceByEventWaitHandle
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class SingleInstanceWindow : Window
+    {
+        internal delegate void ProcessArgDelegate(string arg);
+        internal static ProcessArgDelegate ProcessArg;
+
+        public SingleInstanceWindow()
+        {
+            ProcessArg = delegate(string arg)
+            {
+                ArgsRun.Text = arg;
+            };
+
+            Initialized += delegate(object sender, EventArgs e) {
+                ArgsRun.Text = (string)Application.Current.Resources[WpfSingleInstance.StartArgKey];
+                Application.Current.Resources.Remove(WpfSingleInstance.StartArgKey);
+            };
+
+            InitializeComponent();
+        }
+    }
+}
--- WpfSingleInstanceByEventWaitHandle/WpfSingleInstance.cs
+++ WpfSingleInstanceByEventWaitHandle/WpfSingleInstance.cs
@@ -6,136 +6,127 @@ using System.IO.IsolatedStorage;
 
 namespace WpfSingleInstanceByEventWaitHandle
 {
-	public static class WpfSingleInstance
-	{
+    public static class WpfSingleInstance
+    {
+        internal static void Make(string name, Application app)
+        {
+            EventWaitHandle eventWaitHandle = null;
+            string eventName = Environment.MachineName + "-" + name;
 
-		internal static void Make(String name, Application app)
-		{
+            bool isFirstInstance = false;
 
-			EventWaitHandle eventWaitHandle = null;
-			String eventName = Environment.MachineName + "-" + name;
+            try
+            {
+                eventWaitHandle = EventWaitHandle.OpenExisting(eventName);
+            }
+            catch
+            {
+                // This code only runs on the first instance.
+                isFirstInstance = true;
+            }
 
-			bool isFirstInstance = false;
+            if (isFirstInstance)
+            {
+                eventWaitHandle = new EventWaitHandle(
+                    false,
+                    EventResetMode.AutoReset,
+                    eventName);
 
-			try
-			{
-				eventWaitHandle = EventWaitHandle.OpenExisting(eventName);
-			}
-			catch
-			{
-				// This code only runs on the first instance.
-				isFirstInstance = true;
-			}
+                ThreadPool.RegisterWaitForSingleObject(eventWaitHandle,
+                    waitOrTimerCallback, app, Timeout.Infinite, false);
 
-			if (isFirstInstance)
-			{
-				eventWaitHandle = new EventWaitHandle(
-					false,
-					EventResetMode.AutoReset,
-					eventName);
-
-				ThreadPool.RegisterWaitForSingleObject(eventWaitHandle, waitOrTimerCallback, app, Timeout.Infinite, false);
-
-				// not need more
-				eventWaitHandle.Close();
-
-
-				// !!! delete it if not use
-				setFirstArgs();
-			}
-			else
-			{
-				// !!! delete it if not use
-				setArgs();
+                // Do not need any more.
+                eventWaitHandle.Close();
 
 
-				eventWaitHandle.Set();
-
-				// For that exit no interceptions
-				Environment.Exit(0);
-			}
-		}
-
-
-		private delegate void dispatcherInvoker();
-
-		private static void waitOrTimerCallback(Object state, Boolean timedOut)
-		{
-			Application app = (Application)state;
-			app.Dispatcher.BeginInvoke(
-					new dispatcherInvoker(delegate() {
-						Application.Current.MainWindow.Activate();
-
-						// !!! delete it if not use
-						processArgs();
-
-					}),
-					null
-				);
-		}
+                // !!! delete it if not use
+                setFirstArgs();
+            }
+            else
+            {
+                // !!! delete it if not use
+                setArgs();
 
 
+                eventWaitHandle.Set();
+
+                // For that exit no interceptions
+                Environment.Exit(0);
+            }
+        }
+
+
+        private delegate void dispatcherInvoker();
+
+        private static void waitOrTimerCallback(Object state, Boolean timedOut)
+        {
+            Application app = (Application)state;
+            app.Dispatcher.BeginInvoke(
+                new dispatcherInvoker(delegate ()
+                {
+                    Application.Current.MainWindow.Activate();
+
+                    // !!! delete it if not use
+                    processArgs();
+
+                }),
+                null
+            );
+        }
 
 
 
-		// Args functionality for test purpose and not developed carefuly
-		#region Args
+        // Args functionality for test purpose and not developed carefuly
 
-		internal static readonly object StartArgKey = "StartArg";
+        internal static readonly object StartArgKey = "StartArg";
 
-		private static readonly String isolatedStorageFileName = "SomeFileInTheRoot.txt";
+        private static readonly String isolatedStorageFileName = "SomeFileInTheRoot.txt";
 
-		private static void setArgs()
-		{
-			string[] args = Environment.GetCommandLineArgs();
-			if (1 < args.Length)
-			{
-				IsolatedStorageFile isoStore =
-					IsolatedStorageFile.GetStore(
-						IsolatedStorageScope.User | IsolatedStorageScope.Assembly,
-						null,
-						null);
+        private static void setArgs()
+        {
+            string[] args = Environment.GetCommandLineArgs();
+            if (1 < args.Length)
+            {
+                IsolatedStorageFile isoStore =
+                    IsolatedStorageFile.GetStore(
+                        IsolatedStorageScope.User | IsolatedStorageScope.Assembly,
+                        null,
+                        null);
 
-				IsolatedStorageFileStream isoStream1 = new IsolatedStorageFileStream(isolatedStorageFileName, FileMode.Create, isoStore);
-				StreamWriter sw = new StreamWriter(isoStream1);
-				string arg = args[1];
-				sw.Write(arg);
-				sw.Close();
-			}
-		}
+                IsolatedStorageFileStream isoStream1 = new IsolatedStorageFileStream(isolatedStorageFileName, FileMode.Create, isoStore);
+                StreamWriter sw = new StreamWriter(isoStream1);
+                string arg = args[1];
+                sw.Write(arg);
+                sw.Close();
+            }
+        }
 
+        private static void setFirstArgs()
+        {
+            string[] args = Environment.GetCommandLineArgs();
+            if (1 < args.Length)
+            {
+                Application.Current.Resources[WpfSingleInstance.StartArgKey] = args[1];
+            }
+        }
 
-		private static void setFirstArgs()
-		{
-			string[] args = Environment.GetCommandLineArgs();
-			if (1 < args.Length)
-			{
-				Application.Current.Resources[WpfSingleInstance.StartArgKey] = args[1];
-			}
-		}
-
-
-		private static void processArgs()
-		{
-			IsolatedStorageFile isoStore =
-				IsolatedStorageFile.GetStore(
-					IsolatedStorageScope.User | IsolatedStorageScope.Assembly,
-					null,
-					null);
+        private static void processArgs()
+        {
+            IsolatedStorageFile isoStore =
+                IsolatedStorageFile.GetStore(
+                    IsolatedStorageScope.User | IsolatedStorageScope.Assembly,
+                    null,
+                    null);
 
 
-			IsolatedStorageFileStream isoStream1 = new IsolatedStorageFileStream(isolatedStorageFileName, FileMode.OpenOrCreate, isoStore);
-			StreamReader sr = new StreamReader(isoStream1);
-			string arg = sr.ReadToEnd();
-			sr.Close();
+            IsolatedStorageFileStream isoStream1 = new IsolatedStorageFileStream(isolatedStorageFileName, FileMode.OpenOrCreate, isoStore);
+            StreamReader sr = new StreamReader(isoStream1);
+            string arg = sr.ReadToEnd();
+            sr.Close();
 
-			isoStore.DeleteFile(isolatedStorageFileName);
+            isoStore.DeleteFile(isolatedStorageFileName);
 
-			Window1.ProcessArg(arg);
-		}
-
-		#endregion
-
-
-	}
+            SingleInstanceWindow.ProcessArg(arg);
+        }
+    }
 }
